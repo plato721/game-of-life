cc484b33ed42b9784186ad47fa2e7a62bc8db8ab
Matt Ewell <matt.ewell@gmail.com>
2016-01-02 16:23:30 -0700

    Add another board test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,7 +8,7 @@ describe Board do
 
   context('reading, writing, enumerating') do
     before do
-      pattern = <<-EOF
+      @pattern = <<-EOF
 ---------------
 ----*----------
 ---------------
@@ -26,7 +26,7 @@ describe Board do
 ---------------
 EOF
 
-      @board = Board.new(pattern:pattern)
+      @board = Board.new(pattern:@pattern)
     end
     it 'reads in a pattern' do
 
@@ -34,6 +34,23 @@ EOF
       expect(@board.cell_at([12, 6]).alive?).to be_truthy
       expect(@board.cell_at([12, 7]).alive?).to be_truthy
       expect(@board.cell_at([6, 13]).alive?).to be_truthy
+    end
+
+    it 'reads in a another pattern' do
+      pattern = <<-EOF
+-*--
+**--
+-*-*
+*-**
+EOF
+      board = Board.new(pattern: pattern)
+
+      expect(board.cell_at([0,0]).alive?).to be_falsey
+      expect(board.cell_at([1,0]).alive?).to be_truthy
+    end
+
+    it 'has a string representation' do
+
     end
   end
 end
