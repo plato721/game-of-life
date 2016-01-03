c43cea7a7862b29b3058b32ab4b4f97d1eabd8b2
Matt Ewell <matt.ewell@gmail.com>
2016-01-02 20:16:21 -0700

    Add game initialization with optional board pattern
    Length and width will be determined based on the pattern provided.
    - No error checking of pattern right now

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,20 @@
 class Game
+  def initialize(args = {})
+    @current_board = init_board(args)
+  end
+
+  def init_board(args)
+    return create_board_with(args) if args[:pattern]
+    return create_random_board
+  end
+
+  def create_random_board
+    @current_board = Board.new
+  end
+
+  def create_board_with(args)
+    length = args[:pattern].split("\n").first.length
+    height = args[:pattern].split("\n").length
+    Board.new(pattern: args[:pattern], length: length, height: height)
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,17 @@ require './spec/spec_helper.rb'
 require './lib/game.rb'
 
 describe Game do
-  xit 'can be instantiated' do
+  it 'can be instantiated' do
     expect true
+  end
+
+  it 'can create a board with a pattern' do
+    pattern = <<-EOF
+-*--
+**--
+-*-*
+*-**
+EOF
+    game = Game.new(pattern: pattern)
   end
 end
